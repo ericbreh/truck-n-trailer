f6c6e97b5ead25675b0676b5e4f704360097c223
Eric Chuang <ericchuang94@gmail.com>
2026-03-13 12:00:23 -0700

    esp setup

--- flake.nix
+++ flake.nix
@@ -1,12 +1,14 @@
 {
-  description = "Python Dev Flake";
+  description = "Truck-N-Trailer Dev Flake";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    esp-idf.url = "github:mirrexagon/nixpkgs-esp-dev";
   };
   outputs = {
     nixpkgs,
     flake-utils,
+    esp-idf,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
@@ -14,34 +16,37 @@
         inherit system;
       };
     in {
-      devShells.default = with pkgs;
-        mkShell {
-          buildInputs = [
-            python3
-            uv
-            ipopt
-            python3Packages.pyqt6
-            qt6.qtwayland
-          ];
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-            pkgs.stdenv.cc.cc
-            pkgs.zlib
-            pkgs.glib
-            pkgs.libGL
-            pkgs.libGLU
-            pkgs.libxcb
-            pkgs.libxext
-            pkgs.libx11
-            pkgs.libsm
-            pkgs.libice
-          ];
-          shellHook = ''
-            if [ ! -d ".venv" ]; then
-              uv venv
-            fi
-            source .venv/bin/activate
-            export PYTHONPATH="${python3Packages.pyqt6}/${python3.sitePackages}:$PYTHONPATH"
-          '';
-        };
+      devShells = {
+        default = with pkgs;
+          mkShell {
+            buildInputs = [
+              python3
+              uv
+              ipopt
+              python3Packages.pyqt6
+              qt6.qtwayland
+            ];
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+              pkgs.stdenv.cc.cc
+              pkgs.zlib
+              pkgs.glib
+              pkgs.libGL
+              pkgs.libGLU
+              pkgs.libxcb
+              pkgs.libxext
+              pkgs.libx11
+              pkgs.libsm
+              pkgs.libice
+            ];
+            shellHook = ''
+              if [ ! -d ".venv" ]; then
+                uv venv
+              fi
+              source .venv/bin/activate
+              export PYTHONPATH="${python3Packages.pyqt6}/${python3.sitePackages}:$PYTHONPATH"
+            '';
+          };
+        esp = esp-idf.devShells.${system}.esp-idf-full;
+      };
     });
 }
