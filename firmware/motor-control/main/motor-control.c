d43fc088a30761df70b12da5c510c722f0dfb5c1
Eric Chuang <ericchuang94@gmail.com>
2026-03-16 11:15:03 -0700

    vTaskDelayUntil

--- firmware/motor-control/main/motor-control.c
+++ firmware/motor-control/main/motor-control.c
@@ -17,6 +17,13 @@
 #define LEDC_DUTY_RES LEDC_TIMER_10_BIT
 #define LEDC_FREQUENCY 20000
 
+// Encoder Config
+#define PCNT_HIGH_LIMIT INT16_MAX
+#define PCNT_LOW_LIMIT INT16_MIN
+#define PCNT_GLITCH_FILTER_NS 1000
+#define COUNTS_PER_OUTPUT_REV 910.0f
+#define SAMPLE_PERIOD_MS 100
+
 void init_hw(pcnt_unit_handle_t *pcnt_unit) {
   // Setup PWM Timer
   ledc_timer_config_t ledc_timer = {.speed_mode = LEDC_MODE,
@@ -47,11 +54,12 @@ void init_hw(pcnt_unit_handle_t *pcnt_unit) {
 
   // Setup Encoder
   pcnt_unit_config_t unit_config = {
-      .high_limit = 32767,
-      .low_limit = -32768,
+      .high_limit = PCNT_HIGH_LIMIT,
+      .low_limit = PCNT_LOW_LIMIT,
   };
   pcnt_new_unit(&unit_config, pcnt_unit);
-  pcnt_glitch_filter_config_t filter_config = {.max_glitch_ns = 1000};
+  pcnt_glitch_filter_config_t filter_config = {.max_glitch_ns =
+                                                   PCNT_GLITCH_FILTER_NS};
   pcnt_unit_set_glitch_filter(*pcnt_unit, &filter_config);
 
   // Channel a
@@ -99,21 +107,26 @@ void app_main(void) {
   pcnt_unit_handle_t pcnt_unit = NULL;
   init_hw(&pcnt_unit);
 
+  const TickType_t xFrequency = pdMS_TO_TICKS(SAMPLE_PERIOD_MS);
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
   int count = 0;
   int prev_count = 0;
   int64_t prev_time = esp_timer_get_time();
+
   set_motor_speed(300);
 
   while (1) {
+    vTaskDelayUntil(&xLastWakeTime, xFrequency);
+
     pcnt_unit_get_count(pcnt_unit, &count);
     int64_t curr_time = esp_timer_get_time();
     int delta = count - prev_count;
     float dt = (curr_time - prev_time) / 1000000.0f;
-    float rpm = (delta / 910.0f) * (60.0f / dt);
-    printf("dt=%0.2f enc=%d delta=%d rpm=%0.2f\n", dt, count, delta, rpm);
+    float rpm = (delta / COUNTS_PER_OUTPUT_REV) * (60.0f / dt);
+    printf("dt=%0.3f enc=%d delta=%d rpm=%0.2f\n", dt, count, delta, rpm);
 
     prev_count = count;
     prev_time = curr_time;
-    vTaskDelay(pdMS_TO_TICKS(100));
   }
 }
