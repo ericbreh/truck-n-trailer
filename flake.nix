965ec179f5efc991f6236189ed3079a14f9c644c
Eric Chuang <ericchuang94@gmail.com>
2026-03-26 17:28:53 -0700

    simplify devshells

--- flake.nix
+++ flake.nix
@@ -3,22 +3,55 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
     esp-idf.url = "github:mirrexagon/nixpkgs-esp-dev";
   };
 
-  outputs = inputs @ {flake-parts, ...}:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+  outputs = {
+    nixpkgs,
+    esp-idf,
+    ...
+  }: let
+    systems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    devShells = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      core = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          python3
+          uv
+          ipopt
+          python3Packages.pyqt6
+        ];
 
-      imports = [
-        ./core/shell.nix
-        ./firmware/shell.nix
-      ];
-    };
+        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+          stdenv.cc.cc
+          zlib
+          glib
+          libGL
+          libGLU
+          libxcb
+          libxext
+          libx11
+          libsm
+          libice
+        ]);
+
+        shellHook = ''
+          if [ ! -d ".venv" ]; then
+            uv venv
+          fi
+          source .venv/bin/activate
+        '';
+      };
+
+      firmware = esp-idf.devShells.${system}.esp-idf-full;
+    });
+  };
 }
